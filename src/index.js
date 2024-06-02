2cd5e0f4651185cbabeafa8edd5baf488b6283c4
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-06-02 17:35:10 -0300

    desafio produto

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  scalar Date
+
+  type Produto {
+    nome: String!
+    preco: Float!
+    desconto: Float
+    precoComDesconto: Float
+  }
+
+  type User {
+    name: String
+    id: ID
+    salario: Float
+  }
+
   type Query {
     ola: String
-    horaAtual: String
+    horaAtual: Date
+    getUsuario: User
+    produtoEmDestaque: Produto
   }
 `;
 
 const resolvers = {
+  User: {
+    salario(User) {
+      return User.salario_real * 2;
+    },
+  },
+
+  Produto: {
+    precoComDesconto(prod) {
+      return prod.preco - prod.preco * prod.desconto;
+    },
+  },
+
   Query: {
     ola() {
-      return "Leonardo Lima 123";
+      return "Leonardo Lima";
     },
     horaAtual() {
-      const date = new Date();
-
-      return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${
-        date.getHours() >= 12 ? "PM" : "AM"
-      }`;
+      return new Date();
+    },
+    getUsuario() {
+      return {
+        name: "Leonardo Lima",
+        id: 123,
+        salario_real: 123.321,
+      };
+    },
+    produtoEmDestaque() {
+      return {
+        nome: "Leite",
+        preco: 10.0,
+        desconto: 0.05,
+      };
     },
   },
 };
