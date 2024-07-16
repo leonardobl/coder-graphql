63dfea388eae48de213e1422ab540fea2b9212a0
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-07-16 09:14:50 -0300

    criacao do metodo para a criacao do perfil

--- src/resolvers/Query/Perfis/index.js
+++ src/resolvers/Query/Perfis/index.js
@@ -4,4 +4,10 @@ module.exports = {
   async perfis() {
     return await db("perfis").select("*");
   },
+
+  async addPerfil(_, perfil) {
+    console.log(perfil);
+    return perfil.data;
+    const hasPerfil = await db("perfis").where({ nome: perfil.nome });
+  },
 };
