044292005d9417666398a2836276290a4a2b958c
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-07-16 13:54:21 -0300

    resolver para filtro do perfil

--- src/resolvers/Query/Perfis/index.js
+++ src/resolvers/Query/Perfis/index.js
@@ -5,9 +5,42 @@ module.exports = {
     return await db("perfis").select("*");
   },
 
-  async addPerfil(_, perfil) {
-    console.log(perfil);
-    return perfil.data;
-    const hasPerfil = await db("perfis").where({ nome: perfil.nome });
+  async addPerfil(_, { data }) {
+    const hasPerfil = await db("perfis").where({ nome: data.nome }).first();
+
+    if (hasPerfil) {
+      const result = await db("perfis")
+        .where({ id: hasPerfil.id })
+        .update({ ...hasPerfil, ...data })
+        .returning("*");
+
+      return result[0];
+    }
+
+    const result = await db("perfis").insert(data).returning("*");
+    return result[0];
+  },
+
+  async perfilFilter(_, { data }) {
+    const { id, nome } = data;
+
+    if (!id && !nome) {
+      throw new Error("Você precisa passar pelo menos um parametro");
+    }
+
+    if (id && nome) {
+      const result = await db("perfis").where({ id, nome }).returning("*");
+      return result[0];
+    }
+
+    if (id) {
+      const result = await db("perfis").where({ id }).returning("*");
+      return result[0];
+    }
+
+    if (nome) {
+      const result = await db("perfis").where({ nome }).returning("*");
+      return result[0];
+    }
   },
 };
